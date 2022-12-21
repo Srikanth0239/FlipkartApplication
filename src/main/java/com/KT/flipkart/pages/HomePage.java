f8d3c3f3eb368319ea075e73701494591d9f98d2
Srikanth0239 <balasrikanth.p@gmail.com>
2022-12-21 14:27:59 +0530

    pages are craeted

--- src/main/java/com/KT/flipkart/pages/HomePage.java
+++ src/main/java/com/KT/flipkart/pages/HomePage.java
@@ -1,16 +1,17 @@
 package com.KT.flipkart.pages;
 
-import java.util.Iterator;
 import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
-public class HomePage {
+import com.KT.flipkart.App;
+
+public class HomePage extends App{
 
 	
 	public WebDriver ldriver;
@@ -21,7 +22,7 @@ public class HomePage {
 		PageFactory.initElements(ldriver, this);
 	}
 	
-	@FindBy(xpath="//input[@placeholder='From']")
+	@FindBy(xpath="//input[@id='oneWayOrigin']")
 	WebElement from;
 	@FindBy(xpath="//input[@placeholder='To']")
 	WebElement to;
@@ -38,31 +39,91 @@ public class HomePage {
 	@FindBy(xpath="//input[@placeholder='DD MMM YYYY' and @name='roundTripCalendarReturnDate']")
 	WebElement returntrip;
 	
-	@FindBy(xpath="//table[@class='CalendarMonth_table CalendarMonth_table_1']/tbody/tr/td")
-	List<WebElement> deptdate;
+	@FindBy(xpath="//input[@id='oneWayCalendarDepartDate']")
+	WebElement onewaydate;
 	
 	@FindBy(xpath="//button[@id='dd-guest-class-type']")
 	WebElement cabin;
 	@FindBy(xpath="//button[@id='dd-passenger-type']")
 	WebElement passangertype;
+	@FindBy(xpath="(//button[@class='close-btn'])[1]")
+	WebElement clearDestination;
 	
+	@FindBy(xpath="//a[@class='dropdown-item']//span//mark")
+	WebElement cityname;
 	
+	@FindBy(xpath="//a[@class='dropdown-item']//span//mark")
+	WebElement tocity;
+	public void selectFrom(String CityFrom) throws InterruptedException
 	
-	public void selectFrom(String CityFrom)
-	{
-		from.clear();
-		from.sendKeys(CityFrom);
-		Select sele=new Select(from);
-		List<WebElement> cityList=sele.getOptions();
-		
-		for (WebElement Auto : cityList) 
+	{	
+		//System.out.println("city is :"+ CityFrom);
+		//from.click();
+		if (!from.getAttribute("value").isEmpty())
 		{
-			if (Auto.getText().equalsIgnoreCase(CityFrom)) 
-			{
-				Auto.click();
-				break;
-			}
+			System.out.println(from.getText());
+			clearDestination.click();
+		}
+		Thread.sleep(2000);
+		from.sendKeys(CityFrom);
+		String fromcity=cityname.getText();
+		if(fromcity.equalsIgnoreCase(CityFrom))
+		{
+			cityname.click();
 		}
 		
+	}
+	
+	public void selectRT(){
+		
+		roundTrip.click();
+	}
+	
+	public void selectOw()
+	{
+		oneway.click();
+	}
+	
+	public void selectMulticity()
+	{
+		multiCity.click();
+	}
+	
+	
+	public void destination(String destination)
+	{
+		System.out.println(destination);
+		to.click();
+		if (!to.getAttribute("value").isEmpty()) {
+			to.clear();
+		}else {
+			//String flyingto=tocity.getText();
+		to.sendKeys(destination);
+		}
+	}
+	
+	public void outBound(String date){
+		
+		onewaydate.click();
+		date="12/Oct/2002";
+		String[] temp=date.split("/");
+		String dt= temp[0];
+		String month=temp[1];
+		String year=temp[2];
+		
+	}
+	
+	
+	public String autoSugession(WebElement ele)
+	{
+		String cityname = null;
+		List<WebElement> city=ele.findElements(By.xpath("//ul[@class='rbt-menu dropdown-menu show']//li//span/mark"));
+		for (int i = 0; i < city.size(); i++) 
+		{
+			 cityname=city.get(i).getText();
+		
+		}
+		
+		return cityname;
 	}
 }
--- src/main/java/com/KT/flipkart/utlies/GenericMethodes.java
+++ src/main/java/com/KT/flipkart/utlies/GenericMethodes.java
@@ -1,10 +1,13 @@
 package com.KT.flipkart.utlies;
 
 import java.time.Duration;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GenericMethodes {
@@ -34,29 +37,38 @@ public class GenericMethodes {
 			e.printStackTrace();
 		}
 	}
-	
-	public void handleingAlret(WebDriver driver ,String text )
-	{
-		if (text.equalsIgnoreCase("Accept"))
-		{
+
+	public void handleingAlret(WebDriver driver, String text) {
+		if (text.equalsIgnoreCase("Accept")) {
 			driver.switchTo().alert().accept();
-		}else if(text.equalsIgnoreCase("dismiss"))
-		{
+		} else if (text.equalsIgnoreCase("dismiss")) {
 			driver.switchTo().alert().dismiss();
-		}else
-		{
-			String msg=driver.switchTo().alert().getText();
+		} else {
+			String msg = driver.switchTo().alert().getText();
 			System.out.println("Message in alret box is :" + msg);
 		}
-		
+
 	}
-	
-	public WebElement waitForElement(WebElement nameOftheElement , long timeOutofSeconds) {
-	
-		WebDriverWait waitTime=new WebDriverWait(driver, Duration.ofSeconds(timeOutofSeconds));
-		WebElement element=waitTime.until(ExpectedConditions.elementToBeClickable(nameOftheElement));
+
+	public WebElement waitForElement(WebElement nameOftheElement, long timeOutofSeconds) {
+
+		WebDriverWait waitTime = new WebDriverWait(driver, Duration.ofSeconds(timeOutofSeconds));
+		WebElement element = waitTime.until(ExpectedConditions.elementToBeClickable(nameOftheElement));
 		return element;
-		
+
+	}
+
+	public LinkedList<String> dropDown(WebElement element) {
+
+		Select select = new Select(element);
+		List<WebElement> elementlist = select.getOptions();
+		LinkedList<String> valueList = new LinkedList<String>();
+		for (WebElement ele : elementlist) {
+
+			valueList.add(ele.getText());
+		}
+		return valueList;
 	}
 	
+
 }
